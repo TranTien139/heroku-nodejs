a40a2ce66d02db70a60333252a39cef7f89b9aca
Tran Văn Tien <tvtien139@gmail.com>
2018-01-10 23:57:51 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+var ejs = require('ejs');
+var path = require('path');
+
+app.set('views',path.join(__dirname + '/views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', function (req, res) {
-    res.send("xin chao tat ca cac ban");
+    res.render('index.ejs',{
+        fullname: 'Tran Van Tien'
+    });
 });
 
 app.listen(port);
